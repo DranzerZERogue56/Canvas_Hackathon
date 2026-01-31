1fe240b5c53aa18e175fe39519f409be4893e649
montypythonCUW <logan.montgomery@cuw.edu>
2026-01-31 15:45:35 -0600

    Calendar Page

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,16 +1,12 @@
 import React from 'react'
-<<<<<<< HEAD
 import API_Key_Landing from './API_Key_Landing'
 import './App.css'   /* <-- fix: import the CSS so the background & centering apply */
-=======
-import './App.css'
-import HiddenCharInput from './Calendar_Page'
->>>>>>> Logan_Branch
+import CALENDAR_PAGE from './Calendar_Page'
 
 function App() {
   return (
     <main className="app-main">
-      <API_Key_Landing />
+      <CALENDAR_PAGE />
     </main>
   )
 }
