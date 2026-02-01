88e241483a1082824480799081443e753646736d
nhimsel <181262025+nhimsel@users.noreply.github.com>
2026-01-31 18:57:22 -0600

    reimplement api.ts

--- client/src/api.ts
+++ client/src/api.ts
@@ -10,7 +10,63 @@ async function apiCall(endpoint, options = {}) {
   return response.json();
 }
 
-export async function tryApiKey(key) {
+async function getCourses() {
+	try {
+		const result = await apiCall('courses', {
+			method: 'GET', 
+		});
+
+		return result.valid;
+	} catch(error) {
+		console.error('Failed to get courses:', error);
+		return false;
+	}
+}
+
+async function getCourse(id: number) {
+	try {
+		const result = await apiCall(`courses/${id}`', {
+			method: 'POST',
+			headers: { "Content-Type": 'application/json' },
+			body: JSON.stringify({ id }),
+		});
+
+		return result.valid;
+	} catch(error) {
+		console.error('Failed to get course:', error);
+		return false;
+	}
+}
+
+async function getAssignments(course: number) {
+	try{
+		const result = await apiCall(`courses/${course}/assignments`, {
+			method: 'POST',
+			headers: { "Content-Type": 'application/json' },
+			body: JSON.stringify({ course }),
+		});
+		return result.valid;
+	} catch(error) {
+		console.error('Failed to get assignments:', error);
+		return false;
+	}
+}
+
+async function getAssignment(course: number, id: number) {
+	try{
+		const result = await apiCall(`courses/${course}/assignments/${id}`, {
+			method: 'POST',
+			headers: { "Content-Type": 'application/json' },
+			body: JSON.stringify({ course }),
+		});
+		return result.valid;
+	} catch(error) {
+		console.error('Failed to get assignment:', error);
+		return false;
+	}
+}
+
+export async function tryApiKey(key: string) {
   try {
     const result = await apiCall('test-api-key', {
       method: 'POST',
@@ -20,8 +76,8 @@ export async function tryApiKey(key) {
 
     return result.valid; // true or false
   } catch (error) {
-    console.error('API key check failed:', error);
-    return false;
+    	console.error('API key check failed:', error);
+    	return false;
   }
 }
 
