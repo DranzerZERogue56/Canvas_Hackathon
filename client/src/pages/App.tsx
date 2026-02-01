d161df76fe3a89fb56348a31feac67dead15270a
montypythonCUW <logan.montgomery@cuw.edu>
2026-01-31 18:59:07 -0600

    t

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -2,16 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import LeftPanel from "../components/LeftPanel/LeftPanel";
 import MainMenu from "./MainMenu.tsx";
 import ClassPage from "./Class_Page";
+import API_Key_Landing from "./API_Key_Landing.tsx";
 
 export default function App() {
   return (
     <div style={{ display: "flex" }}>
-      <LeftPanel />
 
       <div style={{ flex: 1 }}>
         <Routes>
-          <Route path="/" element={<MainMenu />} />
-          <Route path="/courses" element={<ClassPage />} />
+          <Route path="/" element={<API_Key_Landing/>} />
         </Routes>
       </div>
     </div>
